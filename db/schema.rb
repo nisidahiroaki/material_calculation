c657efbbe71afa5ee32d0a42c1a28a0c94da1b87
nisidahiroaki <57944481+nisidahiroaki@users.noreply.github.com>
2020-01-29 11:50:48 +0900

    ログイン画面、新規登録画面の作成

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_01_28_110550) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
