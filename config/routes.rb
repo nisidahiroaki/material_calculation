14a964e2c6cabcaf53976903724c54eb327dedb5
nisidahiroaki <57944481+nisidahiroaki@users.noreply.github.com>
2020-01-31 16:09:16 +0900

    Userコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root to: 'items#index'
 
   resources :items, only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :users, only: :show
 
   get 'registered', to: 'items#registered'
 
