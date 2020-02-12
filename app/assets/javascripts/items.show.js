a25dd09f0b8e61622a19c4a342c722a0f1c6c7c1
nisidahiroaki <57944481+nisidahiroaki@users.noreply.github.com>
2020-02-12 20:29:47 +0900

    jquery 導入

--- app/assets/javascripts/items.show.js
+++ app/assets/javascripts/items.show.js
@@ -1,14 +1,24 @@
-function calcCut(){
-  // var weight1 = document.getElementById('id01').value;   ⬅︎参考にしたBMIのコード
-  var width1 = gon.item_width
-  // var height1 = document.getElementById('id02').value;   ⬅︎参考にしたBMIのコード
-  var height1 = gon.item_height
-  var width2 = document.getElementById('id01').value;
-  var height2 = document.getElementById('id02').value;
-  width1 = Number(width1);
-  height1 = Number(height1);
-  width2 = Number(width2);
-  height2 = Number(height2);
-  var cut = (width1 * height1) / (width2 * height2);
-  alert("取れる枚数は" + cut + "です。");
-}
+
+// document.getElementById("button").onclick = function() {
+  function calcCut(){
+    // var weight1 = document.getElementById('id01').value;   ⬅︎参考にしたBMIのコード
+    var width1 = gon.item_width
+    // var height1 = document.getElementById('id02').value;   ⬅︎参考にしたBMIのコード
+    var height1 = gon.item_height
+    var width2 = document.getElementById('id01').value;
+    var height2 = document.getElementById('id02').value;
+    width1 = Number(width1);
+    height1 = Number(height1);
+    width2 = Number(width2);
+    height2 = Number(height2);
+
+    if(width2 <= width1 && height2 <= width1 && width2 <= height1 && height2 <= height1){
+      var cut = (width1 * height1) / (width2 * height2);
+      alert("取れる枚数は" + cut + "です。");
+    }else{
+      // document.getElementById("id03").innerHTML = "クリックされた！";
+      alert("選択した生地が小さくて取れません。");
+    };
+
+  };
+// };
